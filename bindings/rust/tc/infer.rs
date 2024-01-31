52027923152a405b92f7cad6a95860149937930a
radrow <radrowicki@gmail.com>
2024-01-31 16:19:53 +0100

    TYPE UNIFICATION .

---
+++ bindings/rust/tc/infer.rs
@@ -0,0 +1,37 @@
+use crate::ast::ast;
+use crate::tc::*;
+use type_env::*;
+use utype::*;
+
+trait Infer<Env> {
+    fn infer(&self, env: &mut Env) -> Type;
+}
+
+
+impl<Env: HasTEnv, T: Infer<Env>> Infer<Env> for ast::Node<T> {
+    fn infer(&self, env: &mut Env) -> Type {
+        in_node(env, self.id, |env_in| {
+            let t = self.node.infer(env_in);
+            env_in.t_env_mut().set_type_here(self.id, t.clone());
+            t
+        })
+    }
+}
+
+impl Infer<type_env::LocalEnv> for ast::Literal {
+    fn infer(&self, _env: &mut LocalEnv) -> Type {
+        match self {
+            ast::Literal::Int{..} => Type::Var("int".to_string()),
+            _ => unimplemented!()
+        }
+    }
+}
+
+impl Infer<type_env::LocalEnv> for ast::Expr {
+    fn infer(&self, env: &mut LocalEnv) -> Type {
+        match self {
+            ast::Expr::Literal{val: lit} => lit.infer(env),
+            _ => unimplemented!()
+        }
+    }
+}
---
+++ bindings/rust/tc/unify.rs
@@ -0,0 +1,8 @@
+use crate::{
+    type_env,
+    utype::*,
+};
+
+pub fn unify<Env: type_env::HasTEnv>(env: Env, t1: &Type, t2: &Type) -> () {
+
+}
